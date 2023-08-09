2686e51b573eab2167c7096589934fd0f3f7ae08
Khabibud <141556434+Khabibud@users.noreply.github.com>
2023-08-09 17:13:09 +0700

    Add files via upload

---
+++ PPNCalculator.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author GCC
+ */
+public interface PPNCalculator {
+    double PAJAK = 0.01; // PPN 1%
+
+    double calculateTotalPrice();
+}
